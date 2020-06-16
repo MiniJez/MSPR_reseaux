9a205b424c17bbc282f3449c66794b5c94e08834
Edouard CLISSON <edouardclisson@gmail.com>
2020-06-16 14:54:35 +0200

    Feat: Add session authentication + website

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const { join } = require('path');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(session({
-    secret: application_secret,
+    secret: 'secret',
     resave: false,
     saveUninitialized: false
 }));
@@ -19,16 +20,25 @@ app.use(session({
 // });
 
 app.post('/login', (req,res) => {
-    let username = req.body.name;
+    let username = req.body.username;
     let password = req.body.password;
 
     if(username === "mspr" && password === "12345"){
         req.session.username = username;
         req.session.isAuthenticated = true;
+        res.redirect('/home')
     } else {
         res.send(403, "Unauthorized")
     }
 });
+
+app.get('/home', (req, res) => {
+    if (req.session.isAuthenticated) {
+        res.sendFile(path.join(__dirname + '/public/website/index.html'))
+    } else {
+        res.send(403, 'Unauthorized')
+    }
+})
 
 app.listen(3333, function () {
     console.log('Example app listening on port 3333!')
