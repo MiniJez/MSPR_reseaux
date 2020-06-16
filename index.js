1a7f98bcf60462520f34b2d7316395ce926f72b1
Edouard CLISSON <edouardclisson@gmail.com>
2020-06-16 12:20:58 +0200

    Feat: serve html file

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+var path = require('path');
 
-app.get('/', function (req, res) {
-    res.send('Hello World! Hot reloading')
-})
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(3333, function () {
     console.log('Example app listening on port 3333!')
