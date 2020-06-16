2079c41718769dc8611a92827532f5a70c72c5e4
Edouard CLISSON <edouardclisson@gmail.com>
2020-06-16 12:35:45 +0200

    Feat: Add auth html page

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 var path = require('path');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static("public"));
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + 'public/index.html'));
+// });
 
 app.listen(3333, function () {
     console.log('Example app listening on port 3333!')
