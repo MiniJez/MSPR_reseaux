7162046184c3edea8a2ac79ff0cd04195ad94c19
Edouard CLISSON <edouardclisson@gmail.com>
2020-06-17 14:20:00 +0200

    Fix: Add AD get user infos + 2FA mail v5

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.get('/login-validation', (req, res) => {
 
 app.post('/login-validation', (req, res) => {
     let code = req.body.code;
-
+    console.log('post login-validation')
     if(code === req.session.code){
         res.sendFile(path.join(__dirname + '/public/website/index.html'))
     } else {
