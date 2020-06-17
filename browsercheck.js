3a3bffef0159269960ce3d1412146e926fb2c8c2
Unknown <wolfla@hotmail.fr>
2020-06-17 15:34:23 +0200

    Feat : add sqlite

--- browsercheck.js
+++ browsercheck.js
@@ -1,10 +1,23 @@
 var express = require('express')
 var useragent = require('express-useragent')
+var sqlite = require("sqlite3")
 
 module.exports.checkBrowser = function (req, res, next) {
-    console.log('LOGGED');
-    var source = req.headers['user-agent']
+    var source = req.headers['user-agent'];
     var ua = useragent.parse(source);
-    console.log(ua.browser);
+    var browser = ua.browser;
+
+    // console.log(req.session.username);
+
+    var db = new sqlite.Database("browsers.db3");
+    db.each("SELECT * FROM browsers", function(err,row){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(row.login+" utilise habituellement : "+row.navigator+"("+row.count+" utilisations)")
+        }
+    });
+
+    console.log(browser);
     next()
 }
