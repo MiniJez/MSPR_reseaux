1cb6ea595a68bc9d6d573104bbabad3c0a39342a
Edouard CLISSON <edouardclisson@gmail.com>
2020-06-18 17:09:42 +0200

    Fix: browser check fct

--- email.js
+++ email.js
@@ -1,16 +1,8 @@
-'use strict'
-
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 
 const sendEmail = (usermail, subject, mainText) => {
     console.log('inside send email')
-    if (subject === null) { // if the value is null have a defulat string there ( forgot to put it in the function)
-        subject = 'New mail from portail.chatelet.fr'
-    }
-    if (subject === null) { // if the value is null have a defulat string there ( forgot to put it in the function)
-        mainText = 'This is the body of the mail'
-    }
 
     var transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -30,13 +22,11 @@ const sendEmail = (usermail, subject, mainText) => {
         console.log('inside callback sendemail')
         if (error) {
             console.log(error);
-        }
-        if(info) {
-            console.log(info)
-        } 
-        else {
+        } else {
             console.log('Email sent: ' + info.response);
         }
+
+        transporter.close();
     });
 }
 
