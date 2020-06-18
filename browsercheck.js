b529ac3ad1886c30e1f6e9151d3a9ecb1a08c3ca
Unknown <wolfla@hotmail.fr>
2020-06-18 14:12:23 +0200

    Refactor: Extract from module

--- browsercheck.js
+++ browsercheck.js
@@ -5,47 +5,6 @@ const mail = require('./email');
 
 
 module.exports.checkBrowser = function (req, res, next) {
-    console.log("--- Browser check ---")
-    var source = req.headers['user-agent'];
-    var ua = useragent.parse(source);
-    var actualBrowser = ua.browser;
-    console.log("Actual browser : "+actualBrowser)
     
-    var username = req.session.username.split("@")[0];
-    console.log("Username : "+username)
-
-    var db = new sqlite.Database("database.db3");    
-    db.each("SELECT COUNT(*) as IsExist FROM browsers WHERE login = '"+username+"'", function(err,row){
-        if(err){
-            console.log(err);
-        }else{
-            console.log("Is user exists : "+row)
-            if (row.IsExist == 0) {
-                console.log("insert")
-                db.run("INSERT INTO browsers VALUES ('"+username+"','"+actualBrowser+"')");
-            }else{
-                db.each("SELECT * FROM browsers WHERE login = '"+username+"'", function(err,item){
-                    if(err){
-                        console.log(err);
-                    }else{
-                        console.log("Last browser : "+item.navigator)
-                        if(item.navigator != actualBrowser){
-                            console.log("update")
-                            mail.sendEmail(req.session.email, "Connexion avec un nouveau navigateur à portail.chatelet.fr", "You have a new connection with " + actualBrowser + ", if it's not you, please contact the support !")
-                            db.run("UPDATE browsers SET navigator = '"+(actualBrowser)+"' WHERE login = '"+username+"'");
-                            req.session.isNewBrowser = true;
-                        }else{
-                            req.session.isNewBrowser = false;
-                        }
-                    }
-                });
-    
-                
-            }
-        }
-    });
-
-
-    console.log("--- --- ---")
     next()
 }
--- index.js
+++ index.js
@@ -89,10 +89,9 @@ app.get('/login-validation', (req, res) => {
             res.redirect('/login')
         } else {
             req.session.email = user.mail
-            console.log("here1")
-            app.use(browsercheck.checkBrowser);
-            console.log("here2")
             
+            BrowserCheck();
+
             req.session.code = Math.floor(100000 + Math.random() * 900000)
             mail.sendEmail(req.session.email, "Code de vérification pour portail.chatelet.fr", "Your validation code is : " + req.session.code)
             console.log(req.session.email)
@@ -131,3 +130,48 @@ app.get('*', (req, res) => {
 app.listen(3333, function () {
     console.log('Example app listening on port 3333!')
 });
+
+function BrowserCheck(){
+    console.log("--- Browser check ---")
+    var source = req.headers['user-agent'];
+    var ua = useragent.parse(source);
+    var actualBrowser = ua.browser;
+    console.log("Actual browser : "+actualBrowser)
+    
+    var username = req.session.username.split("@")[0];
+    console.log("Username : "+username)
+
+    var db = new sqlite.Database("database.db3");    
+    db.each("SELECT COUNT(*) as IsExist FROM browsers WHERE login = '"+username+"'", function(err,row){
+        if(err){
+            console.log(err);
+        }else{
+            console.log("Is user exists : "+row)
+            if (row.IsExist == 0) {
+                console.log("insert")
+                db.run("INSERT INTO browsers VALUES ('"+username+"','"+actualBrowser+"')");
+            }else{
+                db.each("SELECT * FROM browsers WHERE login = '"+username+"'", function(err,item){
+                    if(err){
+                        console.log(err);
+                    }else{
+                        console.log("Last browser : "+item.navigator)
+                        if(item.navigator != actualBrowser){
+                            console.log("update")
+                            mail.sendEmail(req.session.email, "Connexion avec un nouveau navigateur à portail.chatelet.fr", "You have a new connection with " + actualBrowser + ", if it's not you, please contact the support !")
+                            db.run("UPDATE browsers SET navigator = '"+(actualBrowser)+"' WHERE login = '"+username+"'");
+                            req.session.isNewBrowser = true;
+                        }else{
+                            req.session.isNewBrowser = false;
+                        }
+                    }
+                });
+    
+                
+            }
+        }
+    });
+
+
+    console.log("--- --- ---")
+}
