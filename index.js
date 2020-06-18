96cfd01b972538221d3c962f5a7b436bf8f18537
Edouard CLISSON <edouardclisson@gmail.com>
2020-06-18 17:16:24 +0200

    Fix: browser check fct

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.get('/login-validation', (req, res) => {
                 } else {
                     console.log('new browser email')
                     req.session.code = Math.floor(100000 + Math.random() * 900000)
-                    sendEmail(req.session.email, "Connexion avec un nouveau navigateur à portail.chatelet.fr", "You have a new connection with " + req.session.actualBrowser + ", if it's not you, please contact the support ! Your validation code is : " + req.session.code)
+                    sendEmail("lou.bege@epsi.fr", "new browser", "test")
                 }
 
                 console.log(req.session.email)
